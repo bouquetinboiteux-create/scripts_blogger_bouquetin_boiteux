b7e0a8c93ff80f2e21d579e8e71f40d5122aee51
bouquetinboiteux-create <bouquetin.boiteux@gmail.com>
2025-12-25 14:27:11 +0100

    Refactor GPX loading and map initialization

--- leaflet-gpx-blogger.js
+++ leaflet-gpx-blogger.js
@@ -1,88 +1,52 @@
-(function init() {
+(function () {
 
-  var mapDiv = document.getElementById("map");
-  if (!mapDiv) {
-    setTimeout(init, 200);
-    return;
+  console.log("SCRIPT LEAFLET GPX OK");
+
+  function waitForMap() {
+    var mapDiv = document.getElementById("map");
+    if (!mapDiv) {
+      setTimeout(waitForMap, 200);
+      return;
+    }
+
+    if (typeof L === "undefined") {
+      console.error("Leaflet non chargé");
+      return;
+    }
+
+    var gpxUrl = mapDiv.dataset.gpx;
+    if (!gpxUrl) {
+      console.error("data-gpx manquant");
+      return;
+    }
+
+    var map = L.map("map").setView([45, 6], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "&copy; OpenStreetMap"
+    }).addTo(map);
+
+    fetch(gpxUrl)
+      .then(function (r) { return r.text(); })
+      .then(function (text) {
+
+        var xml = new DOMParser().parseFromString(text, "application/xml");
+        var pts = xml.getElementsByTagName("trkpt");
+        var latlngs = [];
+
+        for (var i = 0; i < pts.length; i++) {
+          latlngs.push([
+            parseFloat(pts[i].getAttribute("lat")),
+            parseFloat(pts[i].getAttribute("lon"))
+          ]);
+        }
+
+        var line = L.polyline(latlngs, { color: "red" }).addTo(map);
+        map.fitBounds(line.getBounds());
+
+      });
   }
 
-  if (typeof L === "undefined") {
-    console.error("Leaflet non chargé");
-    return;
-  }
-
-  var gpxUrl = mapDiv.dataset.gpx;
-  if (!gpxUrl) {
-    console.error("data-gpx manquant");
-    return;
-  }
-
-  // ---- CARTE ----
-  var map = L.map("map");
-
-  var osm = L.tileLayer(
-    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    { attribution: "&copy; OpenStreetMap" }
-  );
-
-  var topo = L.tileLayer(
-    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-    { attribution: "&copy; OpenTopoMap" }
-  );
-
-  topo.addTo(map);
-
-  L.control.layers({
-    "Topographique": topo,
-    "OpenStreetMap": osm
-  }).addTo(map);
-
-  // ---- ICÔNE DÉPART / ARRIVÉE ----
-  var redIcon = L.icon({
-    iconUrl:
-      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
-    shadowUrl:
-      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41]
-  });
-
-  // ---- GPX ----
-  fetch(gpxUrl)
-    .then(r => r.text())
-    .then(text => {
-
-      var xml = new DOMParser().parseFromString(text, "application/xml");
-      var pts = xml.getElementsByTagName("trkpt");
-
-      var latlngs = [];
-      var elevations = [];
-
-      for (var i = 0; i < pts.length; i++) {
-        latlngs.push([
-          parseFloat(pts[i].getAttribute("lat")),
-          parseFloat(pts[i].getAttribute("lon"))
-        ]);
-
-        var ele = pts[i].getElementsByTagName("ele")[0];
-        if (ele) elevations.push(parseFloat(ele.textContent));
-      }
-
-      var line = L.polyline(latlngs, {
-        color: "red",
-        weight: 4
-      }).addTo(map);
-
-      map.fitBounds(line.getBounds(), { padding: [40, 40] });
-
-      L.marker(latlngs[0], { icon: redIcon })
-        .addTo(map)
-        .bindPopup("🚩 Départ");
-
-      L.marker(latlngs[latlngs.length - 1], { icon: redIcon })
-        .addTo(map)
-        .bindPopup("🏁 Arrivée");
-
-    });
+  waitForMap();
 
 })();
